73579c129ee93dd58f35bf81871e0e6bae9fb0fb
blackbat13 <blackbat13@gmail.com>
2023-10-23 23:28:34 +0200

    Expanded app, added comments.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# weather-website-tutorial
- 
+# Weather Website Tutorial
+
+This project provides a straightforward interface for users to check the current weather conditions of a specific location. By simply entering the desired location, the user can view temperature details, and the background changes to reflect the current weather.
+
+## Features
+
+- Dynamic background images that change based on the current weather condition.
+- Current temperature display.
+
+## Data Sources
+
+- **Weather Data**: all weather-related information, including temperature and weather conditions, is sourced from the [Open-Meteo API](https://open-meteo.com).
+- **Background Images**: the beautiful background images that adapt to the weather conditions are sourced from [Pexels](https://www.pexels.com).
+
+## Getting Started
+
+To run the project locally:
+1. Clone the repository.
+2. Open `index.html` in a web browser.
+3. Enter a location to view its current weather.
+
+## Acknowledgments
+
+Special thanks to [Pexels](https://www.pexels.com) for providing high-quality images and [Open-Meteo](https://open-meteo.com) for delivering accurate and timely weather data.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,66 @@
+// Asynchronous function to fetch weather data based on a search query
 async function getWeather(data) {
+
+    // Grabbing the search input element by its ID
     let searchInput = document.getElementById("search-input");
+
+    // Getting the value (place) entered by the user in the search input
     let place = searchInput.value;
+
+    // Fetching geographical coordinates for the input place using the geocode API
     let response = await fetch("http://geocode.maps.co/search?q=" + place);
+
+    // Parsing the response to JSON
     let myJson = await response.json();
+
+    // Extracting longitude and latitude for the given place
     let lon = myJson[0]["lon"];
     let lat = myJson[0]["lat"];
+
+    // Fetching weather data for the given coordinates using the open-meteo API
     let weatherResponse = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon + "&current_weather=true");
+
+    // Parsing the weather response to JSON
     let weatherJson = await weatherResponse.json();
+
+    // Extracting current weather data from the response
     let currentWeather = weatherJson["current_weather"];
-    let temperatureEl = document.getElementById("temperature");
+
+    // Grabbing the element to display the temperature by its ID
+    let temperatureEl = document.getElementById("temperature-text");
+
+    // Updating the search input with the complete name of the place for clarity
     place = myJson[0]["display_name"];
     searchInput.value = place;
+
+    // Logging current weather data to console (for debugging or other purposes)
     console.log(currentWeather);
+
+    // Updating the temperature element with the current temperature value
     temperatureEl.innerHTML = currentWeather["temperature"] + " °C";
+
+    // Retrieve the day status (1 for day, 0 for night) from the currentWeather object
+    let isDay = currentWeather["is_day"];
+
+    // Retrieve the weather code that represents the current weather condition
+    let weatherCode = currentWeather["weathercode"];
+
+    // Check the weather condition based on the weather code
+    if (weatherCode >= 95) {
+        // Set background image to 'storm.jpg' and text color to white if there's a storm
+        document.body.style.backgroundImage = "url('./assets/images/storm.jpg')";
+        document.body.style.color = "#fff";
+    } else if (weatherCode >= 61 && weatherCode <= 65) {
+        // Set background image to 'rain.jpg' and text color to black if it's raining
+        document.body.style.backgroundImage = "url('./assets/images/rain.jpg')";
+        document.body.style.color = "#000";
+    } else if (isDay == 0) {
+        // Set background image to 'night.jpg' and text color to white if it's nighttime
+        document.body.style.backgroundImage = "url('./assets/images/night.jpg')";
+        document.body.style.color = "#fff";
+    } else {
+        // Default: set background image to 'clear.jpg' and text color to black
+        document.body.style.backgroundImage = "url('./assets/images/clear.jpg')";
+        document.body.style.color = "#000";
+    }
 }
